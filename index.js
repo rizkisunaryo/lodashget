e0e0a9a8b2d4bd0e78536f1a97a47d64a63aec8c
Rizki Sunaryo <rizkisunaryo@gmail.com>
2019-12-24 08:32:32 +0700

    Handle if whatToGet is null or undefined

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 module.exports = (objOrArr, whatToGet, defaultValue) => {
-    if (!objOrArr) return defaultValue
+  if (!objOrArr || !whatToGet) return defaultValue
 
-    const whatToGetArr = whatToGet
-        .split(/[.[]/)
-        .filter(el => el !== '')
-        .map(el => el.split(/['`\]]/).join(''))
+  const whatToGetArr = whatToGet
+    .split(/[.[]/)
+    .filter(el => el !== '')
+    .map(el => el.split(/['`\]]/).join(''))
 
-    for (let i = 0; i < whatToGetArr.length; i++) {
-        if (objOrArr[whatToGetArr[i]]) {
-            objOrArr = objOrArr[whatToGetArr[i]]
-        } else {
-            return defaultValue
-        }
+  for (let i = 0; i < whatToGetArr.length; i++) {
+    if (objOrArr[whatToGetArr[i]]) {
+      objOrArr = objOrArr[whatToGetArr[i]]
+    } else {
+      return defaultValue
     }
-    return objOrArr
+  }
+  return objOrArr
 }
--- index.test.js
+++ index.test.js
@@ -2,83 +2,116 @@ const lodashGet = require('./')
 const assert = require('assert')
 
 describe('lodashget', () => {
-    it(`should return return 'c' for lodashGet(obj, 'a.b')`, () => {
-        const obj = {
-            a: {
-                b: 'c'
-            }
-        }
-        assert.equal(lodashGet(obj, 'a.b'), 'c')
-    })
+  it(`should return return 'c' for lodashGet(obj, 'a.b')`, () => {
+    const obj = {
+      a: {
+        b: 'c'
+      }
+    }
+    assert.equal(lodashGet(obj, 'a.b'), 'c')
+  })
 
-    it(`should return return '' as default value`, () => {
-        const obj = {
-            a: {
-                b: 'c'
-            }
-        }
-        assert.equal(lodashGet(obj, 'a.b.c', ''), '')
-    })
+  it(`should return return '' as default value`, () => {
+    const obj = {
+      a: {
+        b: 'c'
+      }
+    }
+    assert.equal(lodashGet(obj, 'a.b.c', ''), '')
+  })
 
-    it(`should return 3 for lodashGet(arr, '[0][1][1]')`, () => {
-        const arr = [[1, [2, 3]], 4]
-        assert.equal(lodashGet(arr, '[0][1][1]'), 3)
-    })
+  it(`should return 3 for lodashGet(arr, '[0][1][1]')`, () => {
+    const arr = [[1, [2, 3]], 4]
+    assert.equal(lodashGet(arr, '[0][1][1]'), 3)
+  })
 
-    it(`should return 'saja' for lodashGet(obj, \`a.b[1][2]['coba']\`)`, () => {
-        const obj = {
-            a: {
-                b: [1, [2, 3, {coba: 'saja'}], 5]
-            }
-        }
-        assert.equal(lodashGet(obj, `a.b[1][2]['coba']`), 'saja')
-    })
+  it(`should return 'saja' for lodashGet(obj, \`a.b[1][2]['coba']\`)`, () => {
+    const obj = {
+      a: {
+        b: [1, [2, 3, { coba: 'saja' }], 5]
+      }
+    }
+    assert.equal(lodashGet(obj, `a.b[1][2]['coba']`), 'saja')
+  })
 
-    it(`should return 10 as defaultValue for lodashGet(arr, '[0][1][1].coba.saja')`, () => {
-        const arr = [[1, [2, 3]], 4]
-        assert.equal(lodashGet(arr, '[0][1][1].coba.saja', 10), 10)
-    })
+  it(`should return 10 as defaultValue for lodashGet(arr, '[0][1][1].coba.saja')`, () => {
+    const arr = [[1, [2, 3]], 4]
+    assert.equal(lodashGet(arr, '[0][1][1].coba.saja', 10), 10)
+  })
 
-    it(`should return 'dasar' as defaultValue for lodashGet(obj, \`a.b[1][2]['coba']['lagi']['terus']\`)`, () => {
-        const obj = {
-            a: {
-                b: [1, [2, 3, {coba: 'saja'}], 5]
-            }
-        }
-        assert.equal(lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'), 'dasar')
-    })
+  it(`should return 'dasar' as defaultValue for lodashGet(obj, \`a.b[1][2]['coba']['lagi']['terus']\`)`, () => {
+    const obj = {
+      a: {
+        b: [1, [2, 3, { coba: 'saja' }], 5]
+      }
+    }
+    assert.equal(
+      lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'),
+      'dasar'
+    )
+  })
 
-    it(`should return default if object / array is null`, () => {
-        const obj = null
-        assert.equal(lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'), 'dasar')
-    })
+  it(`should return default if object / array is null`, () => {
+    const obj = null
+    assert.equal(
+      lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'),
+      'dasar'
+    )
+  })
 
-    it(`should return default if object / array is undefined`, () => {
-        const obj = undefined
-        assert.equal(lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'), 'dasar')
-    })
+  it(`should return default if object / array is undefined`, () => {
+    const obj = undefined
+    assert.equal(
+      lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'),
+      'dasar'
+    )
+  })
 
-    it(`should return default if array, but expected like object`, () => {
-        const arr = [[1, [2, 3]], 4]
-        assert.equal(lodashGet(arr, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'), 'dasar')
-    })
+  it(`should return default if array, but expected like object`, () => {
+    const arr = [[1, [2, 3]], 4]
+    assert.equal(
+      lodashGet(arr, `a.b[1][2]['coba']['lagi']['terus']`, 'dasar'),
+      'dasar'
+    )
+  })
 
-    it(`should return undefined if expected doesn't exist, but no default value`, () => {
-        const obj = {
-            a: {
-                b: [1, [2, 3, {coba: 'saja'}], 5]
-            }
-        }
-        assert.equal(lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`), undefined)
-    })
+  it(`should return undefined if expected doesn't exist, but no default value`, () => {
+    const obj = {
+      a: {
+        b: [1, [2, 3, { coba: 'saja' }], 5]
+      }
+    }
+    assert.equal(
+      lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`),
+      undefined
+    )
+  })
 
-    it(`should return undefined if object / array is null, and default value is not set`, () => {
-        const obj = null
-        assert.equal(lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`), undefined)
-    })
+  it(`should return undefined if object / array is null, and default value is not set`, () => {
+    const obj = null
+    assert.equal(
+      lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`),
+      undefined
+    )
+  })
 
-    it(`should return undefined if object / array is undefined, and default value is not set`, () => {
-        const obj = undefined
-        assert.equal(lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`), undefined)
-    })
+  it(`should return undefined if object / array is undefined, and default value is not set`, () => {
+    const obj = undefined
+    assert.equal(
+      lodashGet(obj, `a.b[1][2]['coba']['lagi']['terus']`),
+      undefined
+    )
+  })
+
+  it(`should return default, if whatToGet is undefined`, () => {
+    const obj = { a: 1 }
+    assert.equal(lodashGet(obj, undefined), undefined)
+    assert.equal(lodashGet(obj, undefined, 'coba'), 'coba')
+  })
+
+  it(`should return default, if whatToGet is null`, () => {
+    const obj = { a: 1 }
+    assert.equal(lodashGet(obj, null), undefined)
+    assert.equal(lodashGet(obj, null, 'coba'), 'coba')
+  })
 })
